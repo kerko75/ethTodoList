2e2dd94c3e9d250e98b3d0cb78766a9593b6847d
keremkos75 <kerem.kos@boostdesign.com.au>
2021-11-03 13:09:44 +1100

    Initial commit.

---
+++ contracts/TodoList.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.5.0;
+
+contract TodoList {
+	uint public taskCount = 0;
+
+	struct Task {
+		uint id;
+		string content;
+		bool completed;
+	}
+
+	mapping(uint => Task) public tasks;
+
+	event TaskCreated(uint id, string content, bool completed);
+	event TaskCompleted(uint id, bool completed);
+
+	constructor() public {
+		createTask("Constructor task");
+	}
+
+	function createTask(string memory _content) public {
+		taskCount++;
+		tasks[taskCount] = Task(taskCount, _content, false);
+		emit TaskCreated(taskCount, _content, false);
+	}
+
+	function toggleCompleted(uint _id) public {
+		tasks[_id].completed = !tasks[_id].completed;
+		emit TaskCompleted(_id, tasks[_id].completed);
+	}
+
+}
